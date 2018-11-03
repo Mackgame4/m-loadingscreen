ab7f9ecb7f8cc2fd42fb0fe1be5de1cce7e95f94
Mackgame4 <42896437+Mackgame4@users.noreply.github.com>
2018-11-03 17:05:40 +0000

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,12 @@
+files {
+    'index.html',
+    'style.css',
+    'bankgothic.ttf',
+    'bg.jpg',
+    'loading.png',
+	'music/Loading.mp3',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
